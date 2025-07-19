849b54849e7f6c698c309ab52b7dd90e800dbc7c
vaibhav Jadhav <132899997+vjdevelope@users.noreply.github.com>
2025-07-19 16:26:31 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb+srv://vaibhavjadhav1374:<Fl9N0cmr4RI15t8J>@cluster0.qxua6jb.mongodb.net/";
+const mongoURI = "mongodb+srv://vaibhavjadhav1374:<Ylnyynyrb886zNmt>@cluster0.qxua6jb.mongodb.net/";
 const connectToMongo = async () => {
   try {
     mongoose.set("strictQuery", false);
