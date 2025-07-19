17d7a3fd803333881b233f2154f308b019e00391
vaibhav Jadhav <132899997+vjdevelope@users.noreply.github.com>
2025-07-19 16:55:22 +0530

    Update UpdateProduct.js

--- Frontend/inventory_management_system/src/components/UpdateProduct.js
+++ Frontend/inventory_management_system/src/components/UpdateProduct.js
@@ -27,7 +27,7 @@ export default function InsertProduct() {
     useEffect(() => {
         const getProduct = async () => {
           try {
-            const res = await fetch(`http://localhost:3001/products/${id}`, {
+            const res = await fetch(`https://inventory-management-system-0nxu.onrender.com/products/${id}`, {
               method: "GET",
               headers: {
                 "Content-Type": "application/json"
@@ -64,7 +64,7 @@ export default function InsertProduct() {
         setError("");
 
         try {
-            const response = await fetch(`http://localhost:3001/updateproduct/${id}`, {
+            const response = await fetch(`https://inventory-management-system-0nxu.onrender.com/updateproduct/${id}`, {
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json"
